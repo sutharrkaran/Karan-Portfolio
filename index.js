4efe577ac7dd52cb92256815edccc667346adf1f
sutharrkaran <karanxsuthar@gmail.com>
2024-02-24 14:59:30 +0530

    reinitialized files

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".ham").addEventListener("click" , function(){
+    document.querySelector(".pages").classList.remove("hide-show");
+    document.querySelector(".left").style.flex = " 0 0 360px";
+});
