8ba42e5af496c323dbb850cda30d4883fdd904ba
sutharrkaran <karanxsuthar@gmail.com>
2024-02-29 15:45:24 +0530

    Refresh

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".ham").addEventListener("click" , function(){
+    document.querySelector(".pages").classList.toggle("hide-show");
+    // document.querySelector(".left").style.flex = " 0 0 360px";
+    // document.querySelector(".ham").style.transform = 'rotate(180deg)';
+});
+
